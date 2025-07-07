5c72aef913d0b9fbd46b62974a422e203c351233
Davidjaoui <david_jaoui@hotmail.com>
2025-07-07 16:00:25 +0000

    creat2

--- Test/test1-ETL.py
+++ Test/test1-ETL.py
@@ -1,19 +1,21 @@
 import pytest
-from etl import extract-data, transform-data, load-data
+import pandas as pd
+from etl import extract_data; #transform_data; #load_data
 
-df = read_csv("data/input_data.csv")
 
-def extract-data(data/input_data.csv):
+
+def testextract_data():
+    df = pd.read_csv("../data/input_data.csv")    
     assert not df.empty
     assert list(df.columns) == ["id","name","age","city","salary"]
     assert df.shape == (5, 7)
     
 
-# def transform-data():
+# def transform_data():
 #     assert subtract(5, 3) == 2
 #     assert subtract(2, 5) == -3
 
-# def load-data():
+# def load_data():
 #     assert multiply(2, 3) == 6
 #     assert multiply(-1, 3) == -3
 
